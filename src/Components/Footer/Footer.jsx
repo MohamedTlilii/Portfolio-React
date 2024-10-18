8e10d5025c46e15657a1da64b5365fd4e8109b10
NAUT5 <Mohamedtlili1995@hotmail.com>
2024-10-18 01:04:50 +0100

    CDC

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,25 +1,29 @@
-import React from 'react'
+import React from 'react';
 import './style.css';
 
 function Footer() {
   return (
-<footer id="footer" class="footer position-relative">
-    <div class="container">
-      <div class="social-links d-flex justify-content-center">
-        <a href="https://www.facebook.com/mohamed.tliliiiii/"><i class="bi bi-facebook" style={{color:"white"}} ></i></a>
-        <a href="https://github.com/MohamedTlilii?tab=repositories"><i class="bi bi-github" style={{color:"white"}}></i></a>
-        <a href="https://www.linkedin.com/in/mohamed-tlili-/"><i class="bi bi-linkedin" style={{color:"white"}}></i></a>
-      </div>
-      <div class="container">
-
-        <div class="credits">
-       
-          Developed by <a>Mohamed Tlili</a>
+    <footer id="footer" className="footer position-relative">
+      <div className="container">
+        <div className="social-links d-flex justify-content-center">
+          <a href="https://www.facebook.com/mohamed.tliliiiii/" target="_blank" rel="noopener noreferrer">
+            <i className="bi bi-facebook" style={{ color: "white" }}></i>
+          </a>
+          <a href="https://github.com/MohamedTlilii?tab=repositories" target="_blank" rel="noopener noreferrer">
+            <i className="bi bi-github" style={{ color: "white" }}></i>
+          </a>
+          <a href="https://www.linkedin.com/in/mohamed-tlili-/" target="_blank" rel="noopener noreferrer">
+            <i className="bi bi-linkedin" style={{ color: "white" }}></i>
+          </a>
+        </div>
+        <div className="container">
+          <div className="credits">
+            Developed by <a href="#">Mohamed Tlili</a>
+          </div>
         </div>
       </div>
-    </div>
-    
-  </footer>  )
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,167 +1,126 @@
-import React from 'react'
-import { GoZoomIn } from "react-icons/go";
+import React from 'react';
 import { CiLink } from "react-icons/ci";
-import { Link, useNavigate } from 'react-router-dom';
-
-
-
+import { useNavigate } from 'react-router-dom';
 import './style.css';
 
 function Portfolio() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  
   return (
-    <section id="portfolio" class="portfolio section">
-
-     
-      <div class="container section-title" data-aos="fade-up">
+    <section id="portfolio" className="portfolio section">
+      <div className="container section-title" data-aos="fade-up">
         <h2>Portfolio</h2>
-
       </div>
 
-      <div class="container">
+      <div className="container">
+        <div className="isotope-layout" data-default-filter="*" data-layout="masonry" data-sort="original-order">
+          <div className="row gy-4 isotope-container" data-aos="fade-up" data-aos-delay="200">
 
-        <div class="isotope-layout" data-default-filter="*" data-layout="masonry" data-sort="original-order">
-
-
-
-
-
-
-
-
-
-
-
-          <div class="row gy-4 isotope-container" data-aos="fade-up" data-aos-delay="200">
-
-
-
-
-
-
-            <div class="col-lg-4 col-md-6 portfolio-item isotope-item ">
-              <img src="assets/img/1708735836555.jpg" class="img-fluid" alt=""/>
-              <div class="portfolio-info">
+            {/* Portfolio Item 1 */}
+            <div className="col-lg-4 col-md-6 portfolio-item isotope-item">
+              <img src="assets/img/1708735836555.jpg" className="img-fluid" alt="EcoRide Electric Scooter"/>
+              <div className="portfolio-info">
                 <h4>EcoRide</h4>
                 <p>Developed E-Commerce EcoRide Electric-Scooter</p>
-                {/* <a href="assets/img/uspjqh5kfaglpycsftrs.jpg" title="Libero X250"
-                  data-gallery="portfolio-gallery-product" class="glightbox preview-link"><i
-                    ><GoZoomIn /></i></a> */}
-                <a onClick={(e)=>{
-                  e.preventDefault()
-                  navigate("/portfoliodetails/1")
-                }} 
-                href="#" 
-                 title="More Details" class="details-link"><i
-                    ><CiLink /></i></a>
+                <a 
+                  onClick={(e) => {
+                    e.preventDefault();
+                    navigate("/portfoliodetails/1");
+                  }} 
+                  role="button" 
+                  title="More Details" 
+                  className="details-link"
+                >
+                  <CiLink />
+                </a>
               </div>
             </div>
 
-
-
-            <div class="col-lg-4 col-md-6 portfolio-item isotope-item ">
-              <img src="assets/img/1714619555840.jpg" class="img-fluid" alt=""/>
-              <div class="portfolio-info">
+            {/* Portfolio Item 2 */}
+            <div className="col-lg-4 col-md-6 portfolio-item isotope-item">
+              <img src="assets/img/1714619555840.jpg" className="img-fluid" alt="LEDJO Ambient Lighting"/>
+              <div className="portfolio-info">
                 <h4>LEDJO</h4>
-                <p>Developed Web site Ledjo Professional Ambient lighting</p>
-                {/* <a href="assets/img/lhxoyylhbzouc7cc0nnm.avif" title="Diamond Tweeter"
-                  data-gallery="portfolio-gallery-app" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a> */}
-                <a onClick={(e)=>{
-                  e.preventDefault()
-                  navigate("/portfoliodetails/2")
-                }} 
-                 href="#" title="More Details" class="details-link"><i
-                    class="bi bi-link-45deg"></i></a>
+                <p>Developed Website for Ledjo Professional Ambient Lighting</p>
+                <a 
+                  onClick={(e) => {
+                    e.preventDefault();
+                    navigate("/portfoliodetails/2");
+                  }} 
+                  role="button" 
+                  title="More Details" 
+                  className="details-link"
+                >
+                  <CiLink />
+                </a>
               </div>
             </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-            <div class="col-lg-4 col-md-6 portfolio-item isotope-item ">
-              <img src="assets/img/ytssss.png" class="img-fluid" alt=""/>
-              <div class="portfolio-info">
+            {/* Portfolio Item 3 */}
+            <div className="col-lg-4 col-md-6 portfolio-item isotope-item">
+              <img src="assets/img/ytssss.png" className="img-fluid" alt="YTS Movies Torrents"/>
+              <div className="portfolio-info">
                 <h4>YTS</h4>
                 <p>YTS Movies Torrents website. Download free movies torrents in 720p, 1080p and 3D quality.</p>
-                {/* <a href="assets/img/lhxoyylhbzouc7cc0nnm.avif" title="Diamond Tweeter"
-                  data-gallery="portfolio-gallery-app" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a> */}
-                <a onClick={(e)=>{
-                  e.preventDefault()
-                  navigate("/portfoliodetails/3")
-                }} 
-                 href="#" title="More Details" class="details-link"><i
-                    class="bi bi-link-45deg"></i></a>
+                <a 
+                  onClick={(e) => {
+                    e.preventDefault();
+                    navigate("/portfoliodetails/3");
+                  }} 
+                  role="button" 
+                  title="More Details" 
+                  className="details-link"
+                >
+                  <CiLink />
+                </a>
               </div>
             </div>
 
-         
-            <div class="col-lg-4 col-md-6 portfolio-item isotope-item ">
-              <img src="assets/img/1.png" class="img-fluid" alt=""/>
-              <div class="portfolio-info">
+            {/* Portfolio Item 4 */}
+            <div className="col-lg-4 col-md-6 portfolio-item isotope-item">
+              <img src="assets/img/1.png" className="img-fluid" alt="BEST-CAR Admin Dashboard"/>
+              <div className="portfolio-info">
                 <h4>BEST-CAR</h4>
                 <p>Developed an admin dashboard for a car rental service.</p>
-                {/* <a href="assets/img/lhxoyylhbzouc7cc0nnm.avif" title="Diamond Tweeter"
-                  data-gallery="portfolio-gallery-app" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a> */}
-                <a onClick={(e)=>{
-                  e.preventDefault()
-                  navigate("/portfoliodetails/4")
-                }} 
-                 href="#" title="More Details" class="details-link"><i
-                    class="bi bi-link-45deg"></i></a>
+                <a 
+                  onClick={(e) => {
+                    e.preventDefault();
+                    navigate("/portfoliodetails/4");
+                  }} 
+                  role="button" 
+                  title="More Details" 
+                  className="details-link"
+                >
+                  <CiLink />
+                </a>
               </div>
             </div>
 
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-            <div class="col-lg-4 col-md-6 portfolio-item isotope-item ">
-              <img src="assets/img/13.png" class="img-fluid" alt=""/>
-              <div class="portfolio-info">
+            {/* Portfolio Item 5 */}
+            <div className="col-lg-4 col-md-6 portfolio-item isotope-item">
+              <img src="assets/img/13.png" className="img-fluid" alt="Centre Descartes Website"/>
+              <div className="portfolio-info">
                 <h4>Centre-Descartes</h4>
                 <p>Developed and deployed a modern, responsive website for Centre Descartes.</p>
-                {/* <a href="assets/img/lhxoyylhbzouc7cc0nnm.avif" title="Diamond Tweeter"
-                  data-gallery="portfolio-gallery-app" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a> */}
-                <a onClick={(e)=>{
-                  e.preventDefault()
-                  navigate("/portfoliodetails/5")
-                }} 
-                 href="#" title="More Details" class="details-link"><i
-                    class="bi bi-link-45deg"></i></a>
+                <a 
+                  onClick={(e) => {
+                    e.preventDefault();
+                    navigate("/portfoliodetails/5");
+                  }} 
+                  role="button" 
+                  title="More Details" 
+                  className="details-link"
+                >
+                  <CiLink />
+                </a>
               </div>
             </div>
 
-
-
-
-
-          
           </div>
-
         </div>
-
       </div>
-      
-
     </section>
-  )
+  );
 }
 
-export default Portfolio
+export default Portfolio;
