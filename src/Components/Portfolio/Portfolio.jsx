980d64c86b96fe27ea33ab848d0dfc6728978788
Mohamed Tlili <144147556+MohamedTlilii@users.noreply.github.com>
2025-01-03 03:41:14 +0100

    Update Portfolio.jsx

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -21,7 +21,7 @@ function Portfolio() {
               <img src="assets/img/1708735836555.jpg" className="img-fluid" alt="EcoRide Electric Scooter" />
               <div className="portfolio-info">
                 <h4>EcoRide</h4>
-                <p>Developed E-Commerce EcoRide Electric-Scooter</p>
+                <p>E-Commerce EcoRide Electric-Scooter</p>
                 <a 
                   href="/portfoliodetails/1" // Added href
                   onClick={(e) => {
@@ -42,7 +42,7 @@ function Portfolio() {
               <img src="assets/img/1714619555840.jpg" className="img-fluid" alt="LEDJO Ambient Lighting" />
               <div className="portfolio-info">
                 <h4>LEDJO</h4>
-                <p>Developed Website for Ledjo Professional Ambient Lighting</p>
+                <p>Ledjo Professional Ambient Lighting</p>
                 <a 
                   href="/portfoliodetails/2" // Added href
                   onClick={(e) => {
@@ -63,7 +63,7 @@ function Portfolio() {
               <img src="assets/img/ytssss.png" className="img-fluid" alt="YTS Movies Torrents" />
               <div className="portfolio-info">
                 <h4>YTS</h4>
-                <p>YTS Movies Torrents website. Download free movies torrents in 720p, 1080p and 3D quality.</p>
+                <p>YTS Movies Torrents website</p>
                 <a 
                   href="/portfoliodetails/3" // Added href
                   onClick={(e) => {
@@ -79,17 +79,19 @@ function Portfolio() {
               </div>
             </div>
 
-            {/* Portfolio Item 4 */}
+           
+
+            {/* Portfolio Item 5 */}
             <div className="col-lg-4 col-md-6 portfolio-item isotope-item">
-              <img src="assets/img/1.png" className="img-fluid" alt="BEST-CAR Admin Dashboard" />
+              <img src="assets/img/13.png" className="img-fluid" alt="Centre Descartes Website" />
               <div className="portfolio-info">
-                <h4>BEST-CAR</h4>
-                <p>Developed an admin dashboard for a car rental service.</p>
+                <h4>Centre-Descartes</h4>
+                <p>Redesigneda modern, responsive website for Centre Descartes</p>
                 <a 
-                  href="/portfoliodetails/4" // Added href
+                  href="/portfoliodetails/5" // Added href
                   onClick={(e) => {
                     e.preventDefault();
-                    navigate("/portfoliodetails/4");
+                    navigate("/portfoliodetails/5");
                   }} 
                   role="button" 
                   title="More Details" 
@@ -100,17 +102,18 @@ function Portfolio() {
               </div>
             </div>
 
-            {/* Portfolio Item 5 */}
-            <div className="col-lg-4 col-md-6 portfolio-item isotope-item">
-              <img src="assets/img/13.png" className="img-fluid" alt="Centre Descartes Website" />
+
+             {/* Portfolio Item 4 */}
+             <div className="col-lg-4 col-md-6 portfolio-item isotope-item">
+              <img src="assets/img/1.png" className="img-fluid" alt="BEST-CAR Admin Dashboard" />
               <div className="portfolio-info">
-                <h4>Centre-Descartes</h4>
-                <p>Developed and deployed a modern, responsive website for Centre Descartes.</p>
+                <h4>BEST-CAR</h4>
+                <p>Admin dashboard for a car rental service</p>
                 <a 
-                  href="/portfoliodetails/5" // Added href
+                  href="/portfoliodetails/4" // Added href
                   onClick={(e) => {
                     e.preventDefault();
-                    navigate("/portfoliodetails/5");
+                    navigate("/portfoliodetails/4");
                   }} 
                   role="button" 
                   title="More Details" 
