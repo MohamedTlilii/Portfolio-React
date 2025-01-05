002b0602e164856879d46be4c7316cab70f691ff
Mohamed Tlili <144147556+MohamedTlilii@users.noreply.github.com>
2025-01-05 16:48:41 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,17 +13,13 @@ function App() {
       title: 'Front-End',
       description: `
         Creation of responsive and dynamic websites using HTML, CSS, and JavaScript.
-        Use of modern frameworks and libraries such as React, Angular.
+        Use of modern frameworks and libraries such as React.
       `,
       points: [
         'Responsive Design with HTML, CSS, and JavaScript.',
         'Harnessing the Power of React for Modern Web Development.',
-        'Angular: Crafting Robust and Feature-rich Web Apps.',
+        'Crafting Robust and Feature-rich Web Apps.',
       ],
-      additionalInfo: `
-        Est reprehenderit voluptatem necessitatibus asperiores neque sed ea illo. Deleniti quam sequi optio iste veniam repellat odit. Aut pariatur itaque nesciunt fuga.
-        Sunt rem odit accusantium omnis perspiciatis officia. Laboriosam aut consequuntur recusandae mollitia doloremque est architecto cupiditate ullam. Quia est ut occaecati fuga. Distinctio ex repellendus eveniet velit sint quia sapiente cumque. Et ipsa perferendis ut nihil. Laboriosam vel voluptates tenetur nostrum. Eaque iusto cupiditate et totam et quia dolorum in. Sunt molestiae ipsum at consequatur vero. Architecto ut pariatur autem ad non cumque nesciunt qui maxime. Sunt eum quia impedit dolore alias explicabo ea.
-      `,
       lottieSrc: 'https://lottie.host/e7fdeab7-9f70-4685-b435-d702077b508b/MhXdbGC5hC.json',
     },
     {
